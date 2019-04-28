fe6afd9a78708e6e6c456559d2dd1095048005a6
骑士救兵 <xusteed@163.com>
2019-04-28 16:59:57 +0800

    page156

---
+++ ch12/sexpr/decode.go
@@ -0,0 +1,21 @@
+package sexpr
+
+import (
+	"fmt"
+	"text/scanner"
+)
+
+type lexer struct {
+	scan  scanner.Scanner
+	token rune // the current token
+}
+
+func (lex *lexer) next()        { lex.token = lex.scan.Scan() }
+func (lex *lexer) text() string { return lex.scan.TokenText() }
+
+func (lex *lexer) consume(want rune) {
+	if lex.token != want { // NOTE: Not an example of good error handling.
+		panic(fmt.Sprintf("got %q, want %q", lex.text(), want))
+	}
+	lex.next()
+}
---
+++ output/expression_evaluator/eval/ast.go
@@ -0,0 +1,34 @@
+package eval
+
+// Expr: 算术表达式
+type Expr interface {
+	// 返回表达式在 env 上下文下的值
+	Eval(env Env) float64
+}
+
+// Var 表示一个变量，比如：x.
+type Var string
+
+// Env 变量到数值的映射关系
+type Env map[Var]float64
+
+// literal 是一个数字常量，比如：3.1415926
+type literal float64
+
+// unary 表示一元操作符表达式，比如：-x
+type unary struct {
+	op rune // one of '+', '-'
+	x  Expr
+}
+
+// binary 表示二元操作符表达式，比如：x+y.
+type binary struct {
+	op   rune // one of '+', '-', '*', '/'
+	x, y Expr
+}
+
+// call 表示函数调用表达式，比如：sin(x).
+type call struct {
+	fn   string // one of "pow", "sin", "sqrt"
+	args []Expr
+}
